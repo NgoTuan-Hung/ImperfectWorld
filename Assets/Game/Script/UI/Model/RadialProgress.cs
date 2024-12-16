d37653afa5fafdb3a91b4ed49e1c00e2c665d073
NgoTuan-Hung <hung.nt207608@sis.hust.edu.vn>
2024-12-16 15:33:08 +0700

    Fixed the problem with render texture and ui toolkit, now our health bar should be render correctly

--- Assets/Game/Script/UI/Model/RadialProgress.cs
+++ Assets/Game/Script/UI/Model/RadialProgress.cs
@@ -5,15 +5,41 @@ using UnityEngine.UIElements;
 public class RadialProgress : MonoBehaviour 
 {
 	UIDocument uIDocument;
-	RadialProgressUI radialProgressUI;
-	private void Awake() 
+	RadialProgressUI radialProgressUIInner;
+	RenderTexture renderTexture;
+	[SerializeField] private float progress = 0.1f;
+	Material material;
+	private void OnEnable() 
 	{
 		uIDocument = GetComponent<UIDocument>();
-		radialProgressUI = uIDocument.rootVisualElement.Q<RadialProgressUI>(name: "radial-progress-inner");
+		
+		/*  */
+		uIDocument.panelSettings = Instantiate(Resources.Load<PanelSettings>("RadialProgress"));
+		renderTexture = Resources.Load<RenderTexture>("RadialProgressRenderTexture");
+        renderTexture = new RenderTexture(renderTexture)
+        {
+            name = "RenderTexture #" + gameObject.name
+        };
+        uIDocument.panelSettings.targetTexture = renderTexture;
+		material = GetComponent<MeshRenderer>().material;
+		material.mainTexture = renderTexture;
+		/*  */
+		
+		radialProgressUIInner = uIDocument.rootVisualElement.Q<RadialProgressUI>(name: "radial-progress-inner");	
+	}
+	
+	[SerializeField] private bool change = false;
+	private void Update() 
+	{
+		if (change)
+		{
+			change = false;
+			SetProgress(progress);
+		}
 	}
 	
 	public void SetProgress(float progress)
 	{
-		radialProgressUI.Progress = progress;
+		radialProgressUIInner.Progress = progress;
 	}
 }
